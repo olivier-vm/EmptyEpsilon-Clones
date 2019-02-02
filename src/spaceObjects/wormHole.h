f8629b6270cacb7d9f0ce2790c188758d74a4cca
olivier-vm <github@dragonfly.be>
2019-02-02 11:06:59 +0100

    daid/EmptyEpsilon - Commits on Jan 7, 2019 void onTeleportation(ScriptSimpleCallback callback);
    Merge pull request #613 from czenker/feature-teleportation
    eventListener when a SpaceObject uses a WormHole

--- src/spaceObjects/wormHole.h
+++ src/spaceObjects/wormHole.h
@@ -10,11 +10,12 @@ class WormHole : public SpaceObject, public Updatable
     sf::Vector2f target_position = sf::Vector2f(0.0f, 0.0f);
     float update_delta = 0.0f;
     P<PathPlannerManager>  pathPlanner;
-    
+
     int radar_visual;
     static const int cloud_count = 5;
     NebulaCloud clouds[cloud_count];
 
+    ScriptSimpleCallback on_teleportation;
 public:
     WormHole();
 
@@ -25,10 +26,12 @@ public:
     virtual void drawOnGMRadar(sf::RenderTarget& window, sf::Vector2f position, float scale, bool long_range);
     virtual void update(float delta);
     virtual void collide(Collisionable* target, float force) override;
-    
+
     void setTargetPosition(sf::Vector2f v);   /* Where to WARP to */
     sf::Vector2f getTargetPosition();
-    
+
+    void onTeleportation(ScriptSimpleCallback callback);
+
     virtual string getExportLine() { return "WormHole():setPosition(" + string(getPosition().x, 0) + ", " + string(getPosition().y, 0) + "):setTargetPosition(" + string(target_position.x, 0) + ", " + string(target_position.y, 0) + ")"; }
 };
 
