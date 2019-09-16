7477afd21746b188e6f52890962b6b8969329482
olivier-vm <github@dragonfly.be>
2019-09-16 21:25:56 +0200

    Merge pull request #632 from amir-arad/patches

--- src/spaceObjects/planet.h
+++ src/spaceObjects/planet.h
@@ -18,7 +18,8 @@ public:
     virtual void drawOnGMRadar(sf::RenderTarget& window, sf::Vector2f draw_position, float scale, bool long_range) override;
     virtual void update(float delta) override;
     virtual void collide(Collisionable* target, float force) override;
-    
+    virtual bool canHideInNebula()  override { return false; }
+
     void setPlanetAtmosphereColor(float r, float g, float b);
     void setPlanetAtmosphereTexture(string texture_name);
     void setPlanetSurfaceTexture(string texture_name);
@@ -28,7 +29,7 @@ public:
     void setDistanceFromMovementPlane(float distance_from_movement_plane);
     void setAxialRotationTime(float time);
     void setOrbit(P<SpaceObject> target, float orbit_time);
-    
+
     virtual string getExportLine() { return "Planet():setPosition(" + string(getPosition().x, 0) + ", " + string(getPosition().y, 0) + ")"; }
 
 private:
@@ -41,16 +42,15 @@ private:
     string atmosphere_texture;
     sf::Color atmosphere_color;
     float distance_from_movement_plane;
-    
+
     float axial_rotation_time;
     int32_t orbit_target_id;
     float orbit_time;
     float orbit_distance;
-    
+
     float collision_size;
 
     void updateCollisionSize();
 };
 
 #endif//WORM_HOLE_H
-
