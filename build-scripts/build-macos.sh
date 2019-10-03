45ee4e5276ad2f334eb63029592c7558d5c7240b
olivier-vm <github@dragonfly.be>
2019-10-03 21:15:43 +0200

    Add build scripts

---
+++ build-scripts/build-macos.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Script to build EE Garou Clone variant under Mac OS X, from local git repo
+# Need to have the git repo in github subdirectory from script launching dir (pwd)
+
+# Variables
+EE_BUILD_HOME=`pwd`/build-ee
+EE_BUILD_EE_PATH="${EE_BUILD_HOME}/EmptyEpsilon-Clones"
+EE_BUILD_SP_PATH="${EE_BUILD_HOME}/SeriousProton"
+
+EE_GITHUB_HOME=`pwd`/github
+EE_GITHUB_EE_PATH="${EE_GITHUB_HOME}/EmptyEpsilon-Clones"
+EE_GITHUB_SP_PATH="${EE_GITHUB_HOME}/SeriousProton"
+
+# Create working directory if not existing
+mkdir -p "${EE_BUILD_HOME}"
+
+# Remove previous existing version of application
+rm -rf "${EE_BUILD_EE_PATH}"
+rm -rf "${EE_BUILD_SP_PATH}"
+
+# Copy local github repo into working directory
+# This is done to avoid mofifying the original github repo
+cp -pr "${EE_GITHUB_EE_PATH}" "${EE_BUILD_HOME}/"
+cp -pr "${EE_GITHUB_SP_PATH}" "${EE_BUILD_HOME}/"
+
+# Move to EE directory, select variant Garou Clones, and build
+cd "${EE_BUILD_EE_PATH}"
+git checkout variant/garou
+
+cd "${EE_BUILD_HOME}"
+cmake "${EE_BUILD_EE_PATH}/CMakeLists.txt" -DSERIOUS_PROTON_DIR="${EE_BUILD_SP_PATH}/"
+cd "${EE_BUILD_EE_PATH}"
+make -j > "${EE_BUILD_HOME}/compile.log"
+make -j install >> "${EE_BUILD_HOME}/compile.log"
+
+# Start the game
+"${EE_BUILD_EE_PATH}/EmptyEpsilon.app/Contents/MacOS/EmptyEpsilon"
---
+++ build-scripts/build-win.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Script to buil (under Debian) EE Garou Clone variant for Microsoft Windows
+
+# Install needed packages if not installed
+sudo apt install mingw-w64 cmake
+
+# Variables
+EE_BUILD_HOME=`pwd`
+EE_BUILD_EE_PATH="${EE_BUILD_HOME}/EmptyEpsilon-Clones"
+EE_BUILD_SP_PATH="${EE_BUILD_HOME}/SeriousProton"
+EE_BUILD_CMAKE="${EE_BUILD_EE_PATH}/cmake"
+
+# Clone repos.
+echo "Cloning or updating git repos..."
+
+## Get SeriousProton and EmptyEpsilon.
+if [ ! -d "${EE_BUILD_SP_PATH}" ]; then
+	echo "-   Cloning SeriousProton repo to ${EE_BUILD_SP_PATH}..."
+	git clone https://github.com/daid/SeriousProton.git "${EE_BUILD_SP_PATH}"
+else
+	echo "-   Fetching and merging SeriousProton repo at ${EE_BUILD_SP_PATH}..."
+	( cd "${EE_BUILD_SP_PATH}";
+	git fetch --all && git merge --ff-only; )
+fi
+echo
+
+if [ ! -d "${EE_BUILD_EE_PATH}" ]; then
+	echo "-   Cloning EmptyEpsilon repo to ${EE_BUILD_EE_PATH}..."
+	git clone https://github.com/olivier-vm/EmptyEpsilon-Clones.git "${EE_BUILD_EE_PATH}"
+else
+	echo "-   Fetching and merging EmptyEpsilon repo at ${EE_BUILD_EE_PATH}..."
+	( cd "${EE_BUILD_EE_PATH}";
+	git fetch --all && git merge --ff-only; )
+fi
+echo
+
+# Build EmptyEpsilon.
+
+cd "${EE_BUILD_EE_PATH}"
+## checkout to Garou Clone variant
+git checkout variant/garou
+
+## Build EmptyEpsilon for Windows.
+echo "Building EmptyEpsilon for Windows..."
+cd "${EE_BUILD_EE_PATH}"
+
+if [ ! -d _build_win32 ]; then
+	mkdir _build_win32
+fi
+cd _build_win32
+
+cmake .. -DCMAKE_TOOLCHAIN_FILE="${EE_BUILD_CMAKE}/mingw.toolchain" -DSERIOUS_PROTON_DIR="${EE_BUILD_SP_PATH}/"
+make -j 12 package
