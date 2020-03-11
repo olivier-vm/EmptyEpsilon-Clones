25e2150fa8058d23f29e1b736ec00b48a8d239e8
Daid <daid303@gmail.com>
2020-03-11 08:55:48 +0100

    Traductions

--- src/shipTemplate.cpp
+++ src/shipTemplate.cpp
@@ -336,12 +336,12 @@ string getLocaleSystemName(ESystem system)
     switch(system)
     {
     case SYS_Reactor: return tr("system", "Reactor");
-    case SYS_BeamWeapons: return tr("system", "Beam Weapons");
+    case SYS_LASERWeapons: return tr("system", "Beam Weapons");
     case SYS_MissileSystem: return tr("system", "Missile System");
     case SYS_Maneuver: return tr("system", "Maneuvering");
     case SYS_Impulse: return tr("system", "Impulse Engines");
-    case SYS_Warp: return tr("system", "Warp Drive");
-    case SYS_JumpDrive: return tr("system", "Jump Drive");
+    case SYS_RLS: return tr("system", "Warp Drive");
+    case SYS_WARPDrive: return tr("system", "Jump Drive");
     case SYS_FrontShield: return tr("system", "Front Shield Generator");
     case SYS_RearShield: return tr("system", "Rear Shield Generator");
     default:
