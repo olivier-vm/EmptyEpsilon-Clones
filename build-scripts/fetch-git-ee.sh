341b3bc5d45e64b76ebd9ed4032012f273be6981
olivier-vm <github@dragonfly.be>
2021-07-10 22:20:23 +0200

    chmod +x build scripts

--- build-scripts/fetch-git-ee.sh
+++ build-scripts/fetch-git-ee.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# Script to build (under Debian) EE Garou Clone variant for Microsoft Windows
+# Script to fetch git repo before building (getting latest build script)
 
 # Install needed packages if not installed
-sudo apt install mingw-w64 cmake git build-essential ninja-build zip
+sudo apt install git
 
 # Variables
 EE_BUILD_HOME=`pwd`
 EE_BUILD_EE_PATH="${EE_BUILD_HOME}/EmptyEpsilon-Clones"
 EE_BUILD_SP_PATH="${EE_BUILD_HOME}/SeriousProton"
-EE_BUILD_CMAKE="${EE_BUILD_EE_PATH}/cmake"
 
 # Clone repos.
 echo "Cloning or updating git repos..."
@@ -33,30 +32,3 @@ else
 	git fetch --all && git merge --ff-only; )
 fi
 echo
-
-# SeriousProton: check older compatible release
-
-cd "${EE_BUILD_SP_PATH}"
-#git checkout EE-2019.09.09
-git checkout EE-2020.02.18
-
-# Build EmptyEpsilon.
-
-cd "${EE_BUILD_EE_PATH}"
-## checkout to Garou Clone variant
-git checkout variant/garou
-
-## Build EmptyEpsilon for Windows.
-echo "Building EmptyEpsilon for Windows..."
-cd "${EE_BUILD_EE_PATH}"
-
-if [ ! -d _build_win32 ]; then
-	mkdir _build_win32
-fi
-cd _build_win32
-
-#cmake .. -G Ninja -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_TOOLCHAIN_FILE="${EE_BUILD_CMAKE}/mingw.toolchain" -DSERIOUS_PROTON_DIR="${EE_BUILD_SP_PATH}/"
-#ninja -j 9 package
-
-cmake .. -DCMAKE_MAKE_PROGRAM=make -DCMAKE_TOOLCHAIN_FILE="${EE_BUILD_CMAKE}/mingw.toolchain" -DSERIOUS_PROTON_DIR="${EE_BUILD_SP_PATH}/"
-make -j 9 package
